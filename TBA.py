7de7b8977158a7b7f9fe8da4d87755797f378c17
SolBreaker1 <JBBallard2025@gmail.com>
2024-08-21 14:32:21 +0530

    teams

--- TBA.py
+++ TBA.py
@@ -1,8 +1,7 @@
 import requests
 
-headers = {'X-TBA-Auth-Key': ''}
+headers = {'X-TBA-Auth-Key': 'vnpeIvxNLYf8NABMB55UsdnY8n6L6GY7cYnokmm8q8HAT7JTmF9H9ttzuzVs9Q1t'}
 
 
 def request_event_teams(event_id):
-    return requests.get(f"https://www.thebluealliance.com/api/v3/event/{event_id}/teams/keys", headers).json()
-
+    return requests.get(f"https://www.thebluealliance.com/api/v3/event/{event_id}/teams", headers).json()
